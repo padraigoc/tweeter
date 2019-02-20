f997d2dbe6c1b2e566d610533fd3cb0967eaaaf9
Patrick O Connor <padraigoc@gmail.com>
2019-02-20 07:26:32 +0000

    updating files

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+
+
+  /* event listener for counting characters, this refers to the element on which the event triggered. */
+  /*https://github.com/sam-meech-ward-lighthouse-labs/w03d2-JavaScript-and-Potatoes/blob/breakout/W3D2-Jquery.md*/
+  $('#tweet_textarea').on('keypress', function(event){ //
+    var cs = $(this).val().length;
+    
+    var remaining = 140 - $(tweet_textarea).val().length; /*This can also be used here instead of tweet_textarea*/
+    
+    if(remaining > 0) {
+      $(counter).css('color', 'black');
+    } else {
+      $(counter).css('color', 'red');
+    }
+    $('#counter').text(remaining);
+  
+
+
+
+  })
+
+  
+});
