c89fbcb0d12d3b2a1cc47ffc6f23cac0ba3b9d3a
Patrick O Connor <padraigoc@gmail.com>
2019-02-21 04:23:49 +0000

    updating files

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,4 +4,98 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function () {
 
+    const createTweetElement = function (tweetObj) {
+        /*retrieve the form information*/
+        const element = `<article>
+        <br>
+        <header>
+        <img id="avatar" src="/images/avatar.png">
+        <div id="name">   ${tweetObj.user.name}
+        <span style="float:right; font-size: 15px; font-weight: normal; padding-top: 10px; margin-right: 10px;"> ${tweetObj.user.handle} </span>             
+        </div>                      
+        <br> 
+        <br>
+        <br>           
+        </header>
+        <p>
+        ${tweetObj.content.text}
+        </p>
+        <footer>
+        <p>${tweetObj.createdat}</p>               
+        </footer>
+        </article>`;
+        //create a var for list container holding tweets
+        const $listOfTweets = $('.tweetcontainer');
+        //add the tweets //keyword here is append 
+        $listOfTweets.prepend(element);
+        //return the tweet object
+        return element;
+    }
+
+
+    function renderTweets(tweets) {
+        // loops through tweets
+        // calls createTweetElement for each tweet
+        // takes return value and appends it to the tweets container
+        for (var key in tweets) {
+            createTweetElement(tweets[key]);
+        }
+        return tweets[key];
+    }
+
+
+
+    //load tweets
+    function loadTweets() {
+        //fetching tweets from the http://localhost:8080/tweets page. 
+
+        $.ajax({
+                method: "GET",
+                url: "/tweets",
+            })
+            .done(function (tweets) {
+
+                const $listOfTweets = $('.tweetcontainer');
+                $listOfTweets.empty(); 
+                renderTweets(tweets);
+
+            })
+
+
+    }
+
+    loadTweets()
+
+    //Event Handler 
+    var $button = $('#sendTweet');
+    $button.on('click', function (event) {
+        console.log('Button clicked, performing ajax call...');
+        event.preventDefault();
+        //use jquery for text area and .val() get.length 
+
+      const len = $('#tweet_textarea').val().length;
+
+
+      if(len == "" || null) {
+        alert("Please enter a message");
+      }
+      else if(len > 140){
+          alert("Please enter 140 characters or less");
+      } else {
+        $.ajax({
+                method: "POST",
+                url: "/tweets",
+                data: $('#new-tweet-form').serialize()
+            })
+            .done(function (loadTweets) {
+                console.log('Success: ', loadTweets);
+            })
+        }
+        loadTweets()
+        $('#tweet_textarea').val("");
+    
+    });
+
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,9 @@
+//1. Forces us to not have global (window-level) variables.
+//2. Decouple the html from the JS and dont assume the JS will be appended to the end of the body
+
+
+
 $(document).ready(function() {
-
-
-
   /* event listener for counting characters, this refers to the element on which the event triggered. */
   /*https://github.com/sam-meech-ward-lighthouse-labs/w03d2-JavaScript-and-Potatoes/blob/breakout/W3D2-Jquery.md*/
   $('#tweet_textarea').on('keypress', function(event){ //
@@ -15,11 +17,33 @@ $(document).ready(function() {
       $(counter).css('color', 'red');
     }
     $('#counter').text(remaining);
-  
-
-
 
   })
 
   
 });
+
+
+
+/*//localhost.8080/creatures.json
+/* app.get('/creatures.json (req,res) => {
+    res.send('hello');
+}) */
+
+/* app.get('/creatures.json (req,res) => {
+    const creatures = [
+        {
+        id: 1,
+        name: 'kymara',
+        damage: 70,
+        type: 'flying'
+    }, 
+    {
+        id: 2,
+        name: 'bigfoot',
+        damage: 40,
+        type: 'land'
+    }];
+    res.json(creatures);
+}) 
+*/
